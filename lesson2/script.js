1391b8519a966f2815c2f4646ecc53fd03616fcd
Vadim <vutkin174@gmail.com>
2019-04-14 16:20:49 +0200

    Дописаны циклы

--- lesson2/script.js
+++ lesson2/script.js
@@ -1,6 +1,6 @@
 "use strict";
 
-var money = prompt('Ваш бюджет на месяц?');
+var money = +prompt('Ваш бюджет на месяц?');
 var time = prompt('Введите дату в формате YYYY-MM-DD');
 
 var appData = {
@@ -22,5 +22,39 @@ for (var i = 0; i < 2; i++) {
         }
     };
     
-var budgetDay = 'Ваш бюджет на 1 день' + appData.budget / 30;
-alert(budgetDay);
+appData.moneyPerDay =  appData.budget / 30;
+alert('Ваш бюджет на 1 день' + appData.moneyPerDay);
+
+if (appData.moneyPerDay < 100) {
+    console.log("Минимальный уровень достатка");
+} else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000) {
+    console.log("Средний уровень достатка");
+} else if (appData.moneyPerDay > 2000) {
+    console.log("Высокий уровень достатка");
+} else {
+    console.log("Произошла ошибка");
+}
+/*while (i < 2) {
+    let i = 0;
+    var a: prompt('Введите обязательную статью расходов в этом месяце', '');
+        b: prompt('Во сколько обойдется?', '');
+        i++;
+        if ((typeof(a)) === 'string'&&(typeof(a)) != null&&(typeof(b)) != null &&a != ''&&b != ''&&a.length < 50) {
+            console.log('done');
+            appData.expenses[a] = b;
+        } else {
+
+        }
+
+}*/
+/*do {
+    var a: prompt('Введите обязательную статью расходов в этом месяце', '');
+        b: prompt('Во сколько обойдется?', '');
+        i++;
+        if ((typeof(a)) === 'string'&&(typeof(a)) != null&&(typeof(b)) != null &&a != ''&&b != ''&&a.length < 50) {
+            console.log('done');
+            appData.expenses[a] = b;
+        } else {
+
+        }
+} while (i < 2)*/
