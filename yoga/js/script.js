d266f3598a1b6ffda230da82fc6bcf2e2cb4f7f1
Vadim <vutkin174@gmail.com>
2019-12-07 21:16:47 +0100

    Задание 13

--- yoga/js/script.js
+++ yoga/js/script.js
@@ -108,7 +108,7 @@ window.addEventListener('DOMContentLoaded', function () {
         document.body.style.overflow = '';
     });
 
-    class Options {
+    /*  class Options {
         constructor(height, width, bg, fontSize, textAlign, border) {
             this.height = height;
             this.width = width;
@@ -136,7 +136,55 @@ window.addEventListener('DOMContentLoaded', function () {
     
 const textOne = new Options(200, 100, 'green', 28, 'center');
 
-textOne.frishDiv('Всем привет!');
+textOne.frishDiv('Всем привет!');*/
+    //Form
 
+    let message = {
+        loading: 'Загрузка...',
+        success: 'Спасибо! Скоро мы свяжемся с Вами!',
+        failure: 'Что-то пошло не так...'
+    };
+
+    let form = document.querySelector('.main-form'),
+        input = form.getElementsByTagName('input'),
+        statusMessage = document.createElement('div');
+
+    statusMessage.classList.add('status');
+
+
+    form.addEventListener('submit', function (event) {
+        event.preventDefault();
+        form.appendChild(statusMessage);
+
+        let request = new XMLHttpRequest();
+        request.open('POST', 'server.php');
+        //request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+        request.setRequestHeader('Content-Type', 'application/json; charset=utf-8');
+        let formData = new FormData(form);
+
+        let obj = {};
+        formData.forEach(function (value, key) {
+            obj[key] = value;
+        });
+        let json = JSON.stringify(obj);
+        request.send(json);
+
+
+        request.addEventListener('readystatechange', function () {
+            console.log(request.readyState);
+            if (request.readyState < 4) {
+                statusMessage.innerHTML = message.loading;
+            } else if (request.readyState === 4 && request.status == 200) {
+                statusMessage.innerHTML = message.success;
+
+            } else {
+                statusMessage.innerHTML = message.failure;
+            }
+
+        });
+        for (let i = 0; i < input.length; i++) {
+            input[i].value = '';
+        }
+    });
 
 });
